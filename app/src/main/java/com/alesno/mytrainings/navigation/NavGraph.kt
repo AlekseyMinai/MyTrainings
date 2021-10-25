308d8abf6b0e7a052dc0dc8a98fcfc99aea06d46
AlekseyMinai <aleksejzuke@gmail.com>
2021-10-25 19:44:12 +0300

    Рефаакторинг NavGraph.kt

--- app/src/main/java/com/alesno/mytrainings/navigation/NavGraph.kt
+++ app/src/main/java/com/alesno/mytrainings/navigation/NavGraph.kt
@@ -1,6 +1,7 @@
 package com.alesno.mytrainings.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -28,51 +29,70 @@ fun NavGraph(
             route = startDestination.routePart,
             startDestination = startDestination.route
         ) {
-            composable(
-                route = Destination.Home(Destination.HomeItem.TRAINING_LIST).route,
-            ) {
-                val store = TrainingListStoreFactory.create(appComponent)
-
-                TrainingListScreen(
-                    store = store,
-                    startTraining = { trainingInfoId ->
-                        val route = Destination.Training(trainingTypeId = trainingInfoId).route
-                        navController.navigate(route)
-                    }
-                )
-            }
-
-            composable(
-                route = Destination.Home(Destination.HomeItem.HISTORY).route
-            ) {
-                val store = TrainingHistoryStoreFactories.create(appComponent)
-
-                TrainingHistory(
-                    store = store,
-                    editTraining = { trainingId ->
-                        val route = Destination.Training(trainingId = trainingId).route
-                        navController.navigate(route)
-
-                    }
-                )
-            }
+            trainingListScreen(appComponent, navController)
+            trainingHomeScreen(appComponent, navController)
         }
+        trainingScreen(appComponent, navController)
+    }
+}
 
-        composable(
-            route = Destination.Training().route
-        ) { backStackEntry ->
-            val store = TrainingStoreFactory.create(
-                appComponent = appComponent,
-                arguments = backStackEntry.arguments
-            )
+fun NavGraphBuilder.trainingListScreen(
+    appComponent: AppComponent,
+    navController: NavHostController
+) {
+    composable(
+        route = Destination.Home(Destination.HomeItem.TRAINING_LIST).route,
+    ) {
+        val store = TrainingListStoreFactory.create(appComponent)
 
-            TrainingScreen(
-                store = store,
-                onBackPressed = {
-                    navController.popBackStack()
-                    TrainingComponent.release()
-                }
-            )
-        }
+        TrainingListScreen(
+            store = store,
+            startTraining = { trainingInfoId ->
+                val route = Destination.Training(trainingTypeId = trainingInfoId).route
+                navController.navigate(route)
+            }
+        )
+    }
+}
+
+fun NavGraphBuilder.trainingHomeScreen(
+    appComponent: AppComponent,
+    navController: NavHostController
+) {
+    composable(
+        route = Destination.Home(Destination.HomeItem.HISTORY).route
+    ) {
+        val store = TrainingHistoryStoreFactories.create(appComponent)
+
+        TrainingHistory(
+            store = store,
+            editTraining = { trainingId ->
+                val route = Destination.Training(trainingId = trainingId).route
+                navController.navigate(route)
+
+            }
+        )
+    }
+}
+
+fun NavGraphBuilder.trainingScreen(
+    appComponent: AppComponent,
+    navController: NavHostController
+) {
+    composable(
+        route = Destination.Training().route
+    ) { backStackEntry ->
+        val store = TrainingStoreFactory.create(
+            appComponent = appComponent,
+            arguments = backStackEntry.arguments
+        )
+
+        TrainingScreen(
+            store = store,
+            onBackPressed = {
+                navController.popBackStack()
+                TrainingComponent.release()
+            }
+        )
     }
 }
